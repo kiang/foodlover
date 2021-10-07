19d37a652c0a5ed9c8c0f4bb9cc9c5479f36c57b
kiang <kiange@gmail.com>
2021-10-08 02:34:27 +0800

    add pay_list to geojson for filtering

--- scripts/01_parse.php
+++ scripts/01_parse.php
@@ -9,6 +9,21 @@ $dataPath = $basePath . '/data/point';
 $pairs = [
     '　' => '',
 ];
+$payPairs = [
+    'PI錢包' => 'PI',
+    '(' => ' ',
+    ')' => '',
+    '.' => '',
+    ':' => '',
+    '&nbsp;' => '',
+    '悠遊pay' => '悠遊付',
+    'APPLYPAY' => 'APPLEPAY',
+    '悠游付' => '悠遊付',
+    '創科悠遊卡' => '創科、悠遊卡',
+    '悠遊付及PI' => '悠遊付、PI',
+    '橘子創科悠遊卡' => '橘子、創科、悠遊卡',
+    '邀遊卡' => '悠遊卡',
+];
 $fh = fopen($basePath . '/data/ref.csv', 'r');
 fgetcsv($fh, 2048);
 $ref = [];
@@ -21,6 +36,7 @@ while ($line = fgetcsv($fh, 2048)) {
 }
 $pool = [];
 $missing = [];
+$paylist = [];
 foreach (glob($basePath . '/raw/data/*.json') as $jsonFile) {
     $json = json_decode(file_get_contents($jsonFile), true);
     if (!empty($json)) {
@@ -39,8 +55,20 @@ foreach (glob($basePath . '/raw/data/*.json') as $jsonFile) {
                 if (is_array($point['shop'])) {
                     $point['shop'] = implode('', $point['shop']);
                 }
-                if (is_array($point['pay_list'])) {
-                    $point['pay_list'] = implode(' / ', $point['pay_list']);
+                if(is_string($point['pay_list'])) {
+                    $point['pay_list'] = [$point['pay_list']];
+                }
+                foreach($point['pay_list'] AS $k => $v) {
+                    $v = trim(strtr($v, $payPairs));
+                    if(false !== strpos($v, '、')) {
+                        unset($point['pay_list'][$k]);
+                        $parts = explode('、', $v);
+                        foreach($parts AS $part) {
+                            $point['pay_list'][] = $part;
+                        }
+                    } else {
+                        $point['pay_list'][$k] = $v;
+                    }
                 }
                 if (is_array($point['market_name'])) {
                     $point['market_name'] = implode('/', $point['market_name']);
@@ -56,47 +84,49 @@ foreach (glob($basePath . '/raw/data/*.json') as $jsonFile) {
                             'x' => floatval($ref[$point['market_address']][0]),
                             'y' => floatval($ref[$point['market_address']][1]),
                             'shops' => [],
+                            'pay_list' => [],
                         ];
                     }
                     $isMissing = false;
+                    
                     $pool[$point['city']][$address]['shops'][] = [
                         'shop' => $point['shop'],
-                        'pay_list' => $point['pay_list'],
+                        'pay_list' => implode(' / ', $point['pay_list']),
                     ];
                 } else {
                     $geocodingFile = $geocodingPath . '/' . $address . '.json';
-                    if (!file_exists($geocodingFile)) {
-                        $apiUrl = $config['tgos']['url'] . '?' . http_build_query([
-                            'oAPPId' => $config['tgos']['APPID'], //應用程式識別碼(APPId)
-                            'oAPIKey' => $config['tgos']['APIKey'], // 應用程式介接驗證碼(APIKey)
-                            'oAddress' => $address, //所要查詢的門牌位置
-                            'oSRS' => 'EPSG:4326', //回傳的坐標系統
-                            'oFuzzyType' => '2', //模糊比對的代碼
-                            'oResultDataType' => 'JSON', //回傳的資料格式
-                            'oFuzzyBuffer' => '0', //模糊比對回傳門牌號的許可誤差範圍
-                            'oIsOnlyFullMatch' => 'false', //是否只進行完全比對
-                            'oIsLockCounty' => 'true', //是否鎖定縣市
-                            'oIsLockTown' => 'false', //是否鎖定鄉鎮市區
-                            'oIsLockVillage' => 'false', //是否鎖定村里
-                            'oIsLockRoadSection' => 'false', //是否鎖定路段
-                            'oIsLockLane' => 'false', //是否鎖定巷
-                            'oIsLockAlley' => 'false', //是否鎖定弄
-                            'oIsLockArea' => 'false', //是否鎖定地區
-                            'oIsSameNumber_SubNumber' => 'true', //號之、之號是否視為相同
-                            'oCanIgnoreVillage' => 'true', //找不時是否可忽略村里
-                            'oCanIgnoreNeighborhood' => 'true', //找不時是否可忽略鄰
-                            'oReturnMaxCount' => '0', //如為多筆時，限制回傳最大筆數
-                        ]);
-                        $content = file_get_contents($apiUrl);
-                        $pos = strpos($content, '{');
-                        $posEnd = strrpos($content, '}') + 1;
-                        $resultline = substr($content, $pos, $posEnd - $pos);
-                        if (strlen($resultline) > 10) {
-                            file_put_contents($geocodingFile, substr($content, $pos, $posEnd - $pos));
-                        } else {
-                            echo $content . "\n";
-                        }
-                    }
+                    // if (!file_exists($geocodingFile)) {
+                    //     $apiUrl = $config['tgos']['url'] . '?' . http_build_query([
+                    //         'oAPPId' => $config['tgos']['APPID'], //應用程式識別碼(APPId)
+                    //         'oAPIKey' => $config['tgos']['APIKey'], // 應用程式介接驗證碼(APIKey)
+                    //         'oAddress' => $address, //所要查詢的門牌位置
+                    //         'oSRS' => 'EPSG:4326', //回傳的坐標系統
+                    //         'oFuzzyType' => '2', //模糊比對的代碼
+                    //         'oResultDataType' => 'JSON', //回傳的資料格式
+                    //         'oFuzzyBuffer' => '0', //模糊比對回傳門牌號的許可誤差範圍
+                    //         'oIsOnlyFullMatch' => 'false', //是否只進行完全比對
+                    //         'oIsLockCounty' => 'true', //是否鎖定縣市
+                    //         'oIsLockTown' => 'false', //是否鎖定鄉鎮市區
+                    //         'oIsLockVillage' => 'false', //是否鎖定村里
+                    //         'oIsLockRoadSection' => 'false', //是否鎖定路段
+                    //         'oIsLockLane' => 'false', //是否鎖定巷
+                    //         'oIsLockAlley' => 'false', //是否鎖定弄
+                    //         'oIsLockArea' => 'false', //是否鎖定地區
+                    //         'oIsSameNumber_SubNumber' => 'true', //號之、之號是否視為相同
+                    //         'oCanIgnoreVillage' => 'true', //找不時是否可忽略村里
+                    //         'oCanIgnoreNeighborhood' => 'true', //找不時是否可忽略鄰
+                    //         'oReturnMaxCount' => '0', //如為多筆時，限制回傳最大筆數
+                    //     ]);
+                    //     $content = file_get_contents($apiUrl);
+                    //     $pos = strpos($content, '{');
+                    //     $posEnd = strrpos($content, '}') + 1;
+                    //     $resultline = substr($content, $pos, $posEnd - $pos);
+                    //     if (strlen($resultline) > 10) {
+                    //         file_put_contents($geocodingFile, substr($content, $pos, $posEnd - $pos));
+                    //     } else {
+                    //         echo $content . "\n";
+                    //     }
+                    // }
                     if (file_exists($geocodingFile)) {
                         $json = json_decode(file_get_contents($geocodingFile), true);
                         if (!empty($json['AddressList'][0]['X'])) {
@@ -110,13 +140,13 @@ foreach (glob($basePath . '/raw/data/*.json') as $jsonFile) {
                                     'x' => floatval($json['AddressList'][0]['X']),
                                     'y' => floatval($json['AddressList'][0]['Y']),
                                     'shops' => [],
+                                    'pay_list' => [],
                                 ];
-                                //echo "{$address}/{$json['AddressList'][0]['X']}/{$json['AddressList'][0]['Y']}\n";
                             }
                             $isMissing = false;
                             $pool[$point['city']][$address]['shops'][] = [
                                 'shop' => $point['shop'],
-                                'pay_list' => $point['pay_list'],
+                                'pay_list' => implode(' / ', $point['pay_list']),
                             ];
                         }
                     }
@@ -125,6 +155,14 @@ foreach (glob($basePath . '/raw/data/*.json') as $jsonFile) {
             if ($isMissing) {
                 if (!isset($missing[$point['market_address']])) {
                     $missing[$point['market_address']] = [$point['city'], $point['area']];
+                }
+            } else {
+                foreach($point['pay_list'] AS $pay) {
+                    $pool[$point['city']][$address]['pay_list'][$pay] = true;
+                    if(!isset($paylist[$pay])) {
+                        $paylist[$pay] = 0;
+                    }
+                    ++$paylist[$pay];
                 }
             }
         }
@@ -142,6 +180,7 @@ foreach ($pool as $city => $lv1) {
         mkdir($filePath, 0777, true);
     }
     foreach ($lv1 as $address => $data) {
+        $data['pay_list'] = implode('/', array_keys($data['pay_list']));
         $dataFile = $filePath . '/' . $address . '.json';
         file_put_contents($dataFile, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 
@@ -149,6 +188,7 @@ foreach ($pool as $city => $lv1) {
             'type' => 'Feature',
             'properties' => [
                 'k' => $address,
+                'pay_list' => $data['pay_list'],
             ],
             'geometry' => [
                 'type' => 'Point',
@@ -167,3 +207,4 @@ fputcsv($missingFh, ['address', 'city', 'area', 'x', 'y']);
 foreach ($missing as $address => $data) {
     fputcsv($missingFh, [$address, $data[0], $data[1], '', '']);
 }
+arsort($paylist);
